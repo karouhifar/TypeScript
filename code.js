6d2300ba3e59914f22233c3306845d2bbb65a424
kamyab rouhifar <krouhifar@myseneca.ca>
2022-05-06 23:23:47 -0400

    added Enum

--- code.js
+++ code.js
@@ -1,21 +1,36 @@
-const buttom = document.getElementById("button");
-const num1 = document.getElementById("num1");
-const num2 = document.getElementById("num2");
-const result = document.getElementById("result");
-class Data {
-    constructor(num1, num2) {
-        this.num1 = num1;
-        this.num2 = num2;
+var buttom = document.getElementById("button");
+var num1Dom = document.getElementById("num1");
+var num2Dom = document.getElementById("num2");
+var result = document.getElementById("result");
+var Data = /** @class */ (function () {
+    function Data() {
     }
-    get getNumber() {
-        return this.num1 + this.num2;
-    }
-    set covertToNumber({ num1, num2 }) {
-        this.num1 = +num1;
-        this.num2 = +num2;
-    }
-}
-buttom.addEventListener("click", () => {
-    let data = new Data(+num1.value, +num2.value);
+    Object.defineProperty(Data.prototype, "getNumber", {
+        get: function () {
+            return this.num1 + this.num2;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Data.prototype, "setNumber", {
+        set: function (_a) {
+            var num1 = _a.num1, num2 = _a.num2;
+            this.num1 = +num1;
+            this.num2 = +num2;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Data;
+}());
+var numbers;
+(function (numbers) {
+    numbers[numbers["num1"] = +num1Dom] = "num1";
+    numbers[numbers["num2"] = +num2Dom] = "num2";
+})(numbers || (numbers = {}));
+;
+buttom.addEventListener("click", function () {
+    var data = new Data();
+    data.setNumber(numbers);
     result.innerHTML = data.getNumber;
 });
--- code.ts
+++ code.ts
@@ -1,29 +1,24 @@
 const buttom = document.getElementById("button");
-const num1 = document.getElementById("num1")! as HTMLInputElement;
-const num2 = document.getElementById("num2")! as HTMLInputElement;
+const num1Dom = document.getElementById("num1")! as HTMLInputElement;
+const num2Dom = document.getElementById("num2")! as HTMLInputElement;
 const result = document.getElementById("result")! as Element;
-
 class Data {
     private num1: number
     private num2: number
-    constructor (num1 : number, num2: number ) {
-        this.num1 = num1;
-        this.num2 = num2;
-    }
-    
+    constructor ( ) {}
     get getNumber() : number {
         return this.num1 + this.num2;
     }
-
-    set covertToNumber({num1,num2}) {
+    set setNumber({num1,num2} ) {
         this.num1 = +num1;
         this.num2 = +num2;
-
          }
 }
 
-buttom.addEventListener("click", ()=> {
+enum numbers { num1 = +num1Dom, num2 = +num2Dom};
 
-    let data = new Data(+num1.value,+num2.value);
+buttom.addEventListener("click", ()=> {
+    let data = new Data();
+    data.setNumber(numbers);
     result.innerHTML = data.getNumber as any;
 });
