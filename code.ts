459dab191a10f7a8fd4ea95c05342a6d519aaf69
kamyab rouhifar <krouhifar@myseneca.ca>
2022-05-07 16:13:40 -0400

    updated code

--- code.ts
+++ code.ts
@@ -1,24 +1,52 @@
 const buttom = document.getElementById("button");
 const num1Dom = document.getElementById("num1")! as HTMLInputElement;
 const num2Dom = document.getElementById("num2")! as HTMLInputElement;
-const result = document.getElementById("result")! as Element;
-class Data {
-    private num1: number
-    private num2: number
-    constructor ( ) {}
+const num3Dom = document.getElementById("num3")! as HTMLInputElement;
+const tBody = document.getElementById("dataJSON")! as HTMLElement;
+const result = document.getElementById("result")! as HTMLElement;
+
+interface PersonName {
+    name ?: string
+}
+interface NumData extends PersonName {
+    doubleAmount: (double: number, nameData: string) => string;
+}
+class Data implements NumData {
+
+
+    constructor (private num1:number,private num2:number) {}
     get getNumber() : number {
         return this.num1 + this.num2;
     }
-    set setNumber({num1,num2} ) {
-        this.num1 = +num1;
-        this.num2 = +num2;
-         }
+
+    doubleAmount(double, nameData) : string {
+       let name : PersonName = {name : nameData} as PersonName;
+       console.log(name);
+       console.log(this.getNumber);
+        return this.getNumber * double + " " + name.name;
+    }
 }
 
-enum numbers { num1 = +num1Dom, num2 = +num2Dom};
+async function  getJSONData () {
+   const dataJSON : Response  =  await fetch("./data.json").then();
+    return dataJSON;
+}
+
+
 
 buttom.addEventListener("click", ()=> {
-    let data = new Data();
-    data.setNumber(numbers);
-    result.innerHTML = data.getNumber as any;
+    let data = new Data(+num1Dom.value,+num2Dom.value);
+    let result1 = data.doubleAmount(+num3Dom.value, "Jack");
+    getJSONData().then((json)=> json.json() ).then((data: Array<{name : string; age: Number, hobbies: Array<string | number>}>)=>{
+        let stringData : string = "";
+       for (const dataArray of data ) {
+        stringData += "<tr><td>" + dataArray.name + "</td><td>" + dataArray.age +"</td><td>" + dataArray.hobbies +"</td></tr>";
+       }
+
+       tBody.innerHTML = stringData; 
+    });
+
+    //  or  data.setNumber(numbers);
+    result.innerHTML = result1 as any;
+    // or  result.innerHTML = <any> data.getNumber;
 });
